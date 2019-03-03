eb2e4047949bbbe680c922876b6296614c77da5c
SrabaniS <srabani.suprakash@gmail.com>
2019-03-03 14:22:56 +0000

    my first change programm

---
+++ src/love/programming/MyMainWorkClass.java
@@ -0,0 +1,13 @@
+package love.programming;
+
+public class MyMainWorkClass {
+    public static void main(String[] args) {
+        MyWorkClass1 obj1 = new MyWorkClass1();
+        obj1.a=10;
+        obj1.b=10;
+        obj1.add();
+        System.out.println ("value of C is : " +obj1.c);
+
+    }
+
+}
---
+++ src/love/programming/MyWorkClass1.java
@@ -0,0 +1,14 @@
+package love.programming;
+
+public class MyWorkClass1 {
+    int a;
+    int b;
+    int c;
+    public int add()
+    {
+      c=a+b;
+      return c;
+
+    }
+
+}
